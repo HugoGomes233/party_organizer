8e5917ee0128711750b8bc33a6bff619d17e488c
Hugo dos Santos Gomes <79466168+MrSkulls@users.noreply.github.com>
2025-05-27 21:18:23 +0100

    first commit

---
+++ Client/src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/nav_bar/NavBar.js';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
